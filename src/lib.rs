821468cfa41ba6a7a23db368ddaf58807ae1afba
Logan Buckley <logan.buckley@gmail.com>
2017-05-23 14:47:12 -0400

    Add basic unit test for parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,22 @@
 pub mod datalog;
 pub mod parser;
+
+
+#[cfg(test)]
+mod test {
+
+    use parser::parse_Query;
+
+    fn should_parse(input: &str) -> () {
+        assert!(match parse_Query(input) {
+            Ok(_) => true,
+            Err(_) => false
+        })
+    }
+
+    #[test]
+    fn test_basic_parsing() {
+        should_parse("find ?a where (?a name \"bob\")");
+        should_parse("find ?a where (?a name \"bob\") (?b family:parent ?a)")
+    }
+}
