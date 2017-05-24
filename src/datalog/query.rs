0d22266da8f2114042259902e4f5d941a4dc2306
BurntPizza <xburntpizza@gmail.com>
2017-05-24 13:48:43 -0400

    First test query working! And cleanup.

--- src/datalog/query.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-use datalog::Value;
-
-struct QueryContext {
-    // Map of symbol to tuple positions
-    symbols: HashMap<String, u64>,
-    // The tuples themselves
-    tuples: Vec<Value>
-}
