c6b515db7f266759d57162cba41be632258ae8f0
Logan Buckley <logan.buckley@gmail.com>
2017-07-18 11:23:39 -0400

    Initial implementation of querying through FFI, with Python client

--- logos-ffi/src/lib.rs
+++ logos-ffi/src/lib.rs
@@ -2,9 +2,9 @@ extern crate logos;
 
 use std::ffi::{CStr, CString};
 use std::mem;
-use std::os::raw::{c_void, c_char, c_int};
+use std::os::raw::{c_char, c_int, c_long};
 
-use logos::{Result, Value};
+use logos::{Result, Value, QueryResult};
 use logos::parse_query;
 use logos::db::{Conn, store_from_uri};
 
@@ -30,32 +30,16 @@ pub extern "C" fn connect(uri_ptr: *mut c_char, ret_ptr: *mut *mut Conn) -> c_in
     }
 }
 
-/// The result of a query is an array of arrays, where the nested
-/// arrays are the result tuples. This struct encodes the necessary
-/// information to pass the nested array across FFI boundaries.  This
-/// is tricky because queries return tuples of values, which are enums
-/// and therefore can't be passed across FFI boundaries. Instead we
-/// have to make a C-style tagged union, and include a key indicated
-/// which types the tuple elements should be cast to.
-#[repr(C)]
-pub struct QueryResult {
-    /// The number of tuples returned by the query.
-    num_results: usize,
-    /// The width of each tuple, i.e. how many variables the query bound.
-    tuple_width: usize,
-    /// They key; an array containing a tag for each tuple position.
-    tuple_types: *const ValueTag,
-    /// The results themselves, an array of arrays. The outer array
-    /// represents the set of tuples; each tuple is represented by an
-    /// inner array. The items in the inner array are either pointers
-    /// to variably-sized values (string, idents, timestamps) or
-    /// inlined entity IDs.
-    results: *const *const c_void,
-    /// A field indicating an error. Null when no error.
-    error: *const c_char,
+#[no_mangle]
+/// Drops the connection created by `connect`.
+pub extern "C" fn close(conn: *mut Conn) {
+    unsafe {
+        let _ = Box::from_raw(conn);
+    }
 }
 
-#[repr(u8)]
+#[repr(u64)]
+#[derive(Debug, Clone)]
 pub enum ValueTag {
     Entity = 0,
     Ident = 1,
@@ -63,93 +47,68 @@ pub enum ValueTag {
     Timestamp = 3,
 }
 
-fn value_tag(v: Value) -> ValueTag {
-    match v {
-        Value::String(_) => ValueTag::String,
-        Value::Entity(_) => ValueTag::Entity,
-        Value::Ident(_) => ValueTag::Ident,
-        Value::Timestamp(_) => ValueTag::Timestamp,
-    }
-}
-
-fn to_c_value(v: &Value) -> *const c_void {
-    match *v {
-        Value::String(ref s) => CString::new(s.clone()).unwrap().into_raw() as *const c_void,
-        Value::Entity(logos::Entity(e)) => e as *const c_void,
-        Value::Ident(ref i) => CString::new(i.clone()).unwrap().into_raw() as *const c_void,
-        Value::Timestamp(t) => CString::new(t.to_string()).unwrap().into_raw() as *const c_void,
-    }
-}
-
-unsafe fn format_query_result(result: logos::QueryResult) -> *const QueryResult {
-    let logos::QueryResult(vars, maps) = result;
-    let tuple_width = vars.len();
-    let num_results = maps.len();
-
-    if num_results == 0 {
-        mem::transmute(Box::new(QueryResult {
-                                    num_results: 0,
-                                    tuple_width: 0,
-                                    tuple_types: std::ptr::null(),
-                                    results: std::ptr::null(),
-                                    error: std::ptr::null(),
-                                }))
-    } else {
-        let mut results = vec![];
-        // Set up the tuple key.
-        let tuple_types: Vec<ValueTag> =
-            vars.iter().map(|v| value_tag(maps[0][v].clone())).collect();
-
-        for m in maps {
-            let mut tuple: Vec<*const c_void> = vec![];
-            for var in vars.iter() {
-                tuple.push(to_c_value(&m[&var]));
-            }
-            let tuple_ptr = tuple.as_ptr();
-            mem::forget(tuple_ptr);
-            results.push(tuple_ptr);
+impl<'a> From<&'a Value> for CValue {
+    fn from(v: &Value) -> CValue {
+        match *v {
+            Value::String(ref s) => CValue::string(s),
+            Value::Entity(logos::Entity(e)) => CValue::entity(e),
+            Value::Ident(ref i) => CValue::string(i),
+            Value::Timestamp(t) => CValue::string(&t.to_string()),
         }
-
-        let tuple_types_ptr = tuple_types.as_ptr() as *const _;
-        let results_ptr = results.as_ptr() as *const _ as *const *const c_void;
-
-        mem::forget(tuple_types);
-        mem::forget(results);
-
-        mem::transmute(Box::new(QueryResult {
-                                    num_results,
-                                    tuple_width,
-                                    tuple_types: tuple_types_ptr,
-                                    results: results_ptr,
-                                    error: std::ptr::null(),
-                                }))
     }
 }
 
-unsafe fn format_error_result(e: logos::Error) -> *const QueryResult {
-    mem::transmute(Box::new(QueryResult {
-                                num_results: 0,
-                                tuple_width: 0,
-                                tuple_types: std::ptr::null(),
-                                results: std::ptr::null(),
-                                error: CString::new(format!("{:?}", e)).unwrap().as_ptr(),
-                            }))
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct CValue {
+    tag: ValueTag,
+    string_val: *const c_char,
+    int_val: c_long,
+}
+
+impl CValue {
+    fn string(val: &str) -> CValue {
+        CValue {
+            tag: ValueTag::String,
+            string_val: CString::new(val).unwrap().into_raw(), // FIXME: leaky!
+            int_val: 0,
+        }
+    }
+
+    fn entity(val: i64) -> CValue {
+        CValue {
+            tag: ValueTag::Entity,
+            string_val: CString::default().into_raw(), // leak
+            int_val: val as c_long,
+        }
+    }
 }
 
 #[no_mangle]
-pub extern "C" fn query(ptr: *mut Conn, query: *const c_char) -> *const QueryResult {
+pub extern "C" fn query(
+    ptr: *mut Conn,
+    query: *const c_char,
+    cb: extern "C" fn(num_items: c_int, row: *const CValue),
+) -> c_int {
     let conn: &Conn = unsafe { &*ptr };
     let query_str = unsafe { CStr::from_ptr(query) };
     let db = match conn.db() {
         Ok(db) => db,
-        Err(e) => return unsafe { format_error_result(e) },
+        Err(_) => return -1,
     };
     let q = parse_query(query_str.to_str().unwrap()).unwrap();
 
     match db.query(&q) {
-        Ok(res) => unsafe { format_query_result(res) },
-        Err(e) => unsafe { format_error_result(e) },
+        Ok(QueryResult(vars, rows)) => {
+            for row in rows {
+                let row_vec: Vec<CValue> =
+                    vars.iter().map(|k| row.get(k).unwrap().into()).collect();
+                cb(vars.len() as i32, row_vec.as_ptr())
+            }
+            return 0;
+        }
+        Err(_) => {
+            return -1;
+        }
     }
 }
-
-// FIXME: need a way to destroy a conn!
--- logos.py
+++ logos.py
@@ -5,36 +5,65 @@ logos = cdll.LoadLibrary("logos-ffi/target/debug/liblogosffi.so")
 logos.connect.argtypes = [c_char_p, c_void_p]
 logos.connect.restype = c_int
 
+# ValueTag enum
+(VAL_ENTITY, VAL_IDENT, VAL_STRING, VAL_TIMESTAMP) = (0, 1, 2, 3)
+
+class CValue(Structure):
+    _fields_ = [
+        ("tag", c_int64),
+        ("string_val", c_char_p),
+        ("int_val", c_int64)
+    ]
+
+    def value(self):
+        if self.tag == VAL_ENTITY:
+            return self.int_val
+        elif self.tag == VAL_IDENT:
+            # TODO: return an interned ident type
+            return self.string_val.decode()
+        elif self.tag == VAL_STRING:
+            return self.string_val.decode()
+        elif self.tag == VAL_TIMESTAMP:
+            # TODO: return a real timestamp
+            return self.string_val.decode()
+        else:
+            pass
+          #  raise Exception("Unsupported tag: {}".format(self.tag))
+
+ROW_CALLBACK = CFUNCTYPE(None, c_int32, POINTER(CValue))
+
+def print_row(num_cols, c_val_p):
+    row = []
+    for i in range(num_cols):
+        row.append(c_val_p[i].value())
+    print(row)
+
+logos.query.argtypes = [c_void_p, c_char_p, ROW_CALLBACK]
+
 uri = c_char_p(b"logos:mem://test")
 conn = c_void_p()
 print(logos.connect(uri, byref(conn)))
 
-query = c_char_p(b"find ?e ?a ?v where (?e ?a ?v)")
+query = c_char_p(b"find ?e ?a where (?e ?a ?v)")
 
-print("python -- calling query")
-print(logos.query(conn, query))
+# print("python -- calling query")
+# print(logos.query(conn, query))
+# logos.query.argtypes = [c_void_p, c_char_p]
+# logos.query.restype = POINTER(QueryResult)
 
-# ValueType union
-(VAL_ENTITY, VAL_IDENT, VAL_STRING, VAL_TIMESTAMP) = (0, 1, 2, 3)
+# res = logos.query(conn, query).contents
 
-class QueryResult(Structure):
-    _fields_ = (
-        ("num_results", c_size_t),
-        ("tuple_width", c_size_t),
-        ("tuple_types", POINTER(c_ubyte)),
-        ("results", POINTER(c_void_p)),
-        ("error", c_char_p)
-    )
+class Query(object):
+    def __init__(self, query_string):
+        self.query_string = query_string
 
-class LogosValue(Union):
-    _fields_ = (
-        ("string", c_char_p),
-        ("ident", c_char_p),
-        ("timestamp", c_char_p),
-        ("entity", c_size_t)
-    )
+    def execute(self, conn):
+        self.results = []
+        def row_cb(num_cols, row_ptr):
+            row = []
+            for i in range(num_cols):
+                row.append(row_ptr[i].value())
+            self.results.append(row)
 
-logos.query.argtypes = [c_void_p, c_char_p]
-logos.query.restype = POINTER(QueryResult)
-
-res = logos.query(conn, query).contents
+        logos.query(conn, self.query_string, ROW_CALLBACK(row_cb))
+        return self.results
